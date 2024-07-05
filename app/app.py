b1bd4d5e25a8cced596a9c48bb506a893a57ee18
soysabaeze <ez.eben2013@hotmail.com>
2024-07-05 17:37:14 -0300

    mejoras

--- app/app.py
+++ app/app.py
@@ -1,32 +1,15 @@
-MYSQL_HOST = "3306"
-MYSQL_USER = "root"
-MYSQL_PASSWORD = "220961ez"
-MYSQL_DB = "agencia_viajes"
-
-from flask import Flask, jsonify, request
-import mysql.connector
-
+from flask import Flask
+from flask import render_template
+from flaskext.mysql import MySQL
 app = Flask(__name__)
+mysql = MySQL()
 
-@app.route("/imagenes", methods=["POST"])
-def crear_imagen():
-    # Recibir datos de la imagen y almacenarlos en la base de datos
-    ...
+mysql.init_app (app)
 
-@app.route("/imagenes/<int:imagen_id>", methods=["PUT"])
-def modificar_imagen(imagen_id):
-    # Recibir datos actualizados y modificar la imagen en la base de datos
-    ...
+if __name__ == '__main__':
+   app.run(debug=True)
 
-@app.route("/imagenes", methods=["GET"])
-def obtener_imagenes():
-    # Obtener todas las imágenes de la base de datos y devolverlas en formato JSON
-    ...
-
-@app.route("/imagenes/<int:imagen_id>", methods=["DELETE"])
-def eliminar_imagen(imagen_id):
-    # Eliminar la imagen con el ID especificado de la base de datos
-    ...
-
-if __name__ == "__main__":
-    app.run(debug=True)
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = '220961ez'
+app.config['MYSQL_DATABASE_DB'] = 'agencia_viajes'
